6844e6d448ca9583e1207217564674afcf8147cf
Rifat Khan <rifat6633@gmail.com>
2025-02-18 23:19:29 +0600

    feat: add new icons and fix layout.

--- apps/do-not-use-wp/src/app/(home)/_ScrollSlider.tsx
+++ apps/do-not-use-wp/src/app/(home)/_ScrollSlider.tsx
@@ -2,32 +2,46 @@
 
 import WPTextDesign from "@/components/WPTextDesign";
 import { motion, useScroll, useTransform } from "motion/react";
+import Image from "next/image";
 import { useRef } from "react";
+import { Icons } from "@/lib/icons";
 
 const wordPressFacts = [
   {
     heading: "Free and Open Source",
     subheading: "WordPress is free and open source software.",
+    image: "/wp/free-and-open-source.webp",
+    icon: Icons.OpenSource,
   },
   {
     heading: "No coding",
     subheading: "WordPress is a no-code tool to create websites.",
+    image: "/wp/no-coding.webp",
+    icon: Icons.NoCoding,
   },
   {
     heading: "Themes and Plugins",
     subheading: "WordPress has a vast ecosystem of plugins and themes.",
+    image: "/wp/themes-and-plugins.webp",
+    icon: Icons.Themes,
   },
   {
     heading: "Community",
     subheading: "WordPress has a large and active community.",
+    image: "/wp/community.webp",
+    icon: Icons.Community,
   },
   {
     heading: "Customizable",
     subheading: "WordPress is customizable through plugins.",
+    image: "/wp/customizable.webp",
+    icon: Icons.Customizable,
   },
   {
     heading: "Third-Party Tools",
     subheading: "WordPress integrates with third-party tools through plugins.",
+    image: "/wp/third-party-tools.webp",
+    icon: Icons.Plugins,
   },
 ];
 
@@ -50,12 +64,27 @@ export default function ScrollSlider() {
 
         <motion.div
           style={{ x: translateX }}
-          className="flex w-[2600px] flex-row flex-nowrap items-center justify-center gap-10 overflow-hidden p-20"
+          className="flex w-[2600px] flex-row flex-nowrap items-stretch justify-center gap-10 overflow-hidden p-20"
         >
           {wordPressFacts.map((fact, i) => (
-            <div key={i} className="size-96 flex-shrink-0 border p-5">
-              <h1 className="text-2xl">{fact.heading}</h1>
-              <p>{fact.subheading}</p>
+            <div key={i} className="w-96 flex-shrink-0 border">
+              <div className="p-5">
+                <div className="flex items-center justify-between gap-2">
+                  <h1 className="text-2xl">{fact.heading}</h1>
+                  <fact.icon className="size-7" />
+                </div>
+                <p>{fact.subheading}</p>
+              </div>
+
+              <div className="mt-auto">
+                <Image
+                  src={fact.image}
+                  alt={`Alt of ${fact.heading}`}
+                  width={300}
+                  height={300}
+                  className="w-full object-fill"
+                />
+              </div>
             </div>
           ))}
         </motion.div>
