50cf430c853b93054fe5df0f6005a70e9ec9b457
Rifat Khan <rifat6633@gmail.com>
2025-03-05 13:40:50 +0600

    feat(Trig): open and close nav on clicking.

--- apps/Trig/js/main.js
+++ apps/Trig/js/main.js
@@ -1,6 +1,8 @@
-window.addEventListener("scroll", function () {
-  const navbar = document.querySelector(".menu-btn");
+const navbar = document.querySelector(".menu-btn");
+const navbarClose = document.querySelector(".nav-close");
+const nav = document.querySelector(".nav");
 
+window.addEventListener("scroll", function () {
   if (window.scrollY > 100) {
     navbar.classList.add("menu-btn-bg-black");
     navbar.classList.remove("menu-btn-bg-transparent");
@@ -8,4 +10,14 @@ window.addEventListener("scroll", function () {
     navbar.classList.add("menu-btn-bg-transparent");
     navbar.classList.remove("menu-btn-bg-black");
   }
+});
+
+navbar.addEventListener("click", function () {
+  nav.classList.add("nav-open");
+  nav.classList.remove("nav-close");
+});
+
+navbarClose.addEventListener("click", function () {
+  nav.classList.add("nav-close");
+  nav.classList.remove("nav-open");
 });
