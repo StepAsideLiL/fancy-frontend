bf3ce9391dc9dda8d605c734fcfae4cc958bbc20
Rifat Khan <rifat6633@gmail.com>
2025-03-05 13:58:03 +0600

    refactor(Trig): change consts name.

--- apps/Trig/js/main.js
+++ apps/Trig/js/main.js
@@ -1,18 +1,18 @@
-const navbar = document.querySelector(".menu-btn");
+const navbarBtn = document.querySelector(".menu-btn");
 const navbarCloseBtn = document.querySelector(".nav-close-btn");
 const nav = document.querySelector(".nav");
 
 window.addEventListener("scroll", function () {
   if (window.scrollY > 100) {
-    navbar.classList.add("menu-btn-bg-black");
-    navbar.classList.remove("menu-btn-bg-transparent");
+    navbarBtn.classList.add("menu-btn-bg-black");
+    navbarBtn.classList.remove("menu-btn-bg-transparent");
   } else {
-    navbar.classList.add("menu-btn-bg-transparent");
-    navbar.classList.remove("menu-btn-bg-black");
+    navbarBtn.classList.add("menu-btn-bg-transparent");
+    navbarBtn.classList.remove("menu-btn-bg-black");
   }
 });
 
-navbar.addEventListener("click", function () {
+navbarBtn.addEventListener("click", function () {
   nav.classList.add("nav-open");
   nav.classList.remove("nav-close");
 });
